9ddf73255481da6ade91edf2707a545a1e29deea
Bryan Rivera <bryan.rivera@lognllc.com>
2020-02-20 11:26:19 -0600

    undo

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ class App extends Component {
     return (
       // eslint-disable-next-line react/jsx-filename-extension
       <div className="App">
-        <h1> Bryan Monsters Rolodex </h1>
+        <h1> Monsters Rolodex </h1>
         <SearchBox
           placeholder="search monsters"
           handleChange={this.handleChange}
