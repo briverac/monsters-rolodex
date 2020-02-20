501cf52f2028320473db0d42cdbf9d71953990b8
Bryan Rivera <bryan.rivera@lognllc.com>
2020-02-20 10:57:14 -0600

    Updates

---
+++ precache-manifest.ada5ac41022969aca900fc0521c1de7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a9c7f6a2474a2c53c1cb5a5bc92db56",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "671dfaac5e1d923723ff",
+    "url": "/monsters-rolodex/static/css/main.6e03cdcf.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "671dfaac5e1d923723ff",
+    "url": "/monsters-rolodex/static/js/main.362a5f17.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
